d6b790f8c0f85b21bf6670a1cc76dae761de6196
cyberkrane <cyberkranegmail.com>
2022-11-25 17:53:34 -0300

    se transfirio el boton logout de main a navbar

--- src/app/componentes/main/main.component.ts
+++ src/app/componentes/main/main.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-main',
@@ -10,18 +8,11 @@ import { UserService } from 'src/app/service/user.service';
 export class MainComponent implements OnInit {
 
   constructor(
-    private userService: UserService,
-    private router: Router
+   
   ) { }
 
   ngOnInit(): void {
   }
 
-  onClick(){
-   this.userService.logout()
-   .then(() => {
-    this.router.navigate(['/login'])
-   })
-   .catch(error => console.log(error))
-  }
+
 }
